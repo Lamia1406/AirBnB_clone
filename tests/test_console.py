878db65c2db9954744b4b6a66143d4bfe0cf6128
H-affy <abdullateefadeluola@gmail.com>
2023-11-08 09:31:51 -0800

    tests for console.py update

--- tests/test_console.py
+++ tests/test_console.py
@@ -43,14 +43,14 @@ class TestHBNBCommand_help(unitteste.TestCase):
 
     def test_help_quit(self):
         h = "Quit command to exit the program."
-        with patch("sys.stdout". new=StringIO as output:
-                self.assertFalse(HBNBCommand().onecmd("help quit"))
-                self.assertEqual(h, output.getvalue().strip())
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("help quit"))
+            self.assertEqual(h, output.getvalue().strip())
 
     def test_help_create(self):
-        h = ("Usage: create <class>\n         "
-            "create a new class instance and print its id."
-        with patch("sys.stdout", bew=StringIO()) as output:
+        h = ("Usage: create <class>\n      "
+                "Create a new class instance and prints it id.")
+        with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("help create"))
             self.assertEqual(h, output.getvalue().strip())
 
@@ -61,19 +61,5 @@ class TestHBNBCommand_help(unitteste.TestCase):
             self.assertEqual(h, output.getvalue().strip())
 
     def test_help_show(self):
-        h = ("Usage: show <class> <id> or <class>.show(<id>)\n    "
-            "Display the string representation of a class instance"
-            "of a given id.")
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("help show"))
-            self.assertEqual(h, output.getvalue().strip())
 
-    def test_help_destroy(self):
-        h = ("Usage: destroy <class> or <class>.all()\n       "
-            "DElete a class instance of a given id.")
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("help destroy"))
-            self.assertEqual(h, output.getvalue().strip())
 
-    def test_help_all(self):
-        h = ("
