2c94e8b0353e265d0f8d2efad21a50083471ab25
H-affy <abdullateefadeluola@gmail.com>
2023-11-08 03:56:07 -0800

    Updated

--- tests/test_console.py
+++ tests/test_console.py
@@ -5,6 +5,11 @@ Unittest classes:
     TestHBNBCommand_prompting
     TestHBNBCommand_help
     TestHBNBCommand_exit
+    TestHBNBCommand_create
+    TestHBNBCommand_show
+    TestHBNBCommand_all
+    TestHBNBCommand_destroy
+    TestHBNBCommand_update
     """
 
 import os
@@ -41,3 +46,34 @@ class TestHBNBCommand_help(unitteste.TestCase):
         with patch("sys.stdout". new=StringIO as output:
                 self.assertFalse(HBNBCommand().onecmd("help quit"))
                 self.assertEqual(h, output.getvalue().strip())
+
+    def test_help_create(self):
+        h = ("Usage: create <class>\n         "
+            "create a new class instance and print its id."
+        with patch("sys.stdout", bew=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("help create"))
+            self.assertEqual(h, output.getvalue().strip())
+
+    def test_help_EOF(self):
+        h = "EOF signal to exit the program."
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("help EOF"))
+            self.assertEqual(h, output.getvalue().strip())
+
+    def test_help_show(self):
+        h = ("Usage: show <class> <id> or <class>.show(<id>)\n    "
+            "Display the string representation of a class instance"
+            "of a given id.")
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("help show"))
+            self.assertEqual(h, output.getvalue().strip())
+
+    def test_help_destroy(self):
+        h = ("Usage: destroy <class> or <class>.all()\n       "
+            "DElete a class instance of a given id.")
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("help destroy"))
+            self.assertEqual(h, output.getvalue().strip())
+
+    def test_help_all(self):
+        h = ("
