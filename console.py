f27cfc092fac0a003ccd7b213ce9f001d526e4dd
H-affy <abdullateefadeluola@gmail.com>
2023-11-13 14:12:12 -0800

    Update

--- console.py
+++ console.py
@@ -114,7 +114,7 @@ class HBNBCommand(cmd.Cmd):
         elif "{}.{}".format(argl[0], argl[1]) not in objdict.keys():
             print("** no instance found **")
         else:
-            del onjdict["{}.{}".format(argl[0], argl[1])]
+            del objdict["{}.{}".format(argl[0], argl[1])]
             storage.save()
 
     def do_all(self, arg):
