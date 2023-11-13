8e75199a7050d7e1a3e1cf2dc2b8eed071a0c3fd
H-affy <abdullateefadeluola@gmail.com>
2023-11-13 14:09:03 -0800

    Update

--- console.py
+++ console.py
@@ -103,25 +103,18 @@ class HBNBCommand(cmd.Cmd):
         """usage: destroy <class> <id>. Deletes an instance based on
         the class name and id.
         """
+        argl = parse(arg)
         objdict = storage.all()
-        if type(arg) != str:
-            argl = parse(arg[0])
-            if len(arg) > 1:
-                arg = arg[1].split('"')[1]
-        else:
-            argl = parse(arg)
-            arg = arg.split(" ")[1]
-
         if len(argl) == 0:
             print("** class name missing **")
         elif argl[0] not in HBNBCommand.__classes:
-            print("** class doesn't exist **")
-        elif len(arg) == 1:
+            print("** class doesn't exit **")
+        elif len(argl) == 1:
             print("** instance id missing **")
-        elif f"{argl[0]}.{arg}" not in objdict:
+        elif "{}.{}".format(argl[0], argl[1]) not in objdict.keys():
             print("** no instance found **")
         else:
-            del objdict["{}.{}".format(argl[0], arg)]
+            del onjdict["{}.{}".format(argl[0], argl[1])]
             storage.save()
 
     def do_all(self, arg):
